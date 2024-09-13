75e43640d4bc58e705a2711372c0829bd40a4f60
Priyam <104610119+priyam-03@users.noreply.github.com>
2024-09-13 22:09:10 +0530

    Update route.ts

--- app/api/records/route.ts
+++ app/api/records/route.ts
@@ -2,15 +2,7 @@ import { NextResponse } from 'next/server';
 import {db} from '../../../lib/db';
 export async function GET() {
     try {
-        const records = await db.record.findMany({
-            include: {
-                landslideRecord: {
-                    include: {
-                        locations: true,
-                    },
-                },
-            },
-        });
+        const records = await db.articles_new.findMany();
 
         return NextResponse.json(records);
     } catch (error) {
