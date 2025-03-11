f5930977e995b1f44cc091a87ceee1619f890833
Priyam <priyam.saha2003@gmail.com>
2025-03-11 20:27:47 +0530

    locations page added

---
+++ app/locations/page.tsx
@@ -0,0 +1,15 @@
+import dynamic from "next/dynamic";
+
+// Define a functional React component named `Home`.
+export default function Home() {
+	const MyMap = dynamic(() => import("../../components/MyMap"), {
+		loading: () => <p>A map is loading</p>,
+		ssr: false,
+	});
+
+	// Render the dynamically imported `MyMap` component.
+	return (
+
+		<MyMap />	
+	);
+}
